d9685aad757b1d98838a336d01b27378cb04cc13
Rave <example@example.com>
2018-02-23 20:12:55 +0100

    Init push @1

---
+++ Source/BasementEvolve/BasementEvolve.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "BasementEvolve.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BasementEvolve, "BasementEvolve" );
+
+DEFINE_LOG_CATEGORY(LogBasementEvolve)
+ 
